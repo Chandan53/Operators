29e3506d35e98ed73abe4b1afda5362b398e1ba5
chandan <chandankumar936672@gmail.com>
2025-07-20 19:18:24 +0530

    logical & assignment

---
+++ operator5.java
@@ -0,0 +1,15 @@
+package Operators;
+
+public class operator5 {
+    public static void main(String args[]){
+        // logical operator
+        // && -->  *
+        // || --> +
+        // !
+
+        System.out.println(3>2 && 1!=1); // both condition must be true
+        System.out.println(3>2 || 1!=1); // one condition must be true
+        System.out.println(! (3<2)); // opposite
+    }
+    
+}
---
+++ operator6.java
@@ -0,0 +1,26 @@
+package Operators;
+
+public class operator6 {
+    public static void main(String args[]){
+        // Assignment operator
+        // =
+        // +=
+        // -=
+        // /=
+        // *=
+        int a=10;
+        int b=5;
+
+        a=b;
+        System.out.println(a);
+        b+=a;
+        System.out.println(b);
+        a-=b;
+        System.out.println(a);
+        a*=b;
+        System.out.println(a);
+        // keep on tracking 😅
+        
+    }
+    
+}
