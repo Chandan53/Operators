2db9f017247c690d06819f17875c71a8f43d5a47
chandan <chandankumar936672@gmail.com>
2025-07-20 18:19:53 +0530

    arithmetic Binary

---
+++ operator2.java
@@ -0,0 +1,22 @@
+package Operators;
+
+public class operator2 {
+    public static void main(String args[]){
+        // Arithmetic operator --> binary  & unary
+        // binary --> having two operands
+        // unary --> having only one operand
+        
+        // binary
+        int a=10;
+        int b=5;
+        System.out.println("add " + (a+b));
+        System.out.println("sub " + (a - b));
+        System.out.println("pro " + (a*b));
+        System.out.println("divide " + (a/b));
+        System.out.println("remainder " + (a%b));
+        //System.out.println("sub "+ a-b);
+        
+
+    }
+    
+}
