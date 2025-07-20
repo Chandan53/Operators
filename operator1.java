f3e55b209b65d81c228bfc6dd6ae0fe2359fa453
chandan <chandankumar936672@gmail.com>
2025-07-20 18:11:01 +0530

    operator1

---
+++ operator1.java
@@ -0,0 +1,19 @@
+package Operators;
+
+public class operator1 {
+    public static void main(String args[]){
+        // symbol that tell compiler to perform some operation 
+        // eg -> sum= a + b
+        // a & b is operands
+        // + --> is operator
+
+        // type of operators
+        // arithmetic operators ( binary / unary)
+        // relation operator
+        // logical operator
+        // bitwise operator
+        // assignment operator
+
+    }
+    
+}
